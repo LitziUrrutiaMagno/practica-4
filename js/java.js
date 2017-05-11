e2d46b41b591dabb843ba8fd2dc78b61f07bd931
LitziUrrutiaMagno <andrea_um98@hotmail.com>
2017-05-11 11:14:10 -0500

    calculadora
    se realizo calculadora en base html y js

---
+++ js/java.js
@@ -0,0 +1,31 @@
+function suma(){ 
+    
+    var num1 =Number(document.getElementById('N1').value);
+    var num2 =Number(document.getElementById('N2').value);
+    var sum=num1+num2;
+    alert("El resultado de tu operacion es = "+(sum));
+}
+function resta(){
+        
+        var num1 =Number(document.getElementById('N1').value);
+        var num2 =Number(document.getElementById('N2').value);
+      
+        alert("El resultado de tu operacion es = "+(num1-num2));
+    }
+function multiplicacion(){
+            
+        var num1 =Number(document.getElementById('N1').value);
+        var num2 =Number(document.getElementById('N2').value);
+        
+        alert("El resultado de tu operacion es = "+(num1*num2));
+        }
+function division(){
+                
+        var num1 =Number(document.getElementById('N1').value);
+        var num2 =Number(document.getElementById('N2').value);
+         
+        alert("El resultado de tu operacion es = "+(num1/num2));
+            
+            }
+        
+        
